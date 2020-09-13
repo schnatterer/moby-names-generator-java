a30849f34b7e952d7d624cfab4e5ae8af35e4d32
Johannes Schnatterer <johannes@schnatterer.info>
2020-09-13 13:41:33 +0200

    Make generator more efficient by reusing random

--- processor/src/main/resources/info/schnatterer/mobynamesgenerator/NamesGenerator-template.java
+++ processor/src/main/resources/info/schnatterer/mobynamesgenerator/NamesGenerator-template.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 public final class MobyNamesGenerator {
 
+    private static Random RANDOM = new Random();
+
     private static String[] left = new String[]{
         {{{left}}}
     };
@@ -18,15 +20,14 @@ public final class MobyNamesGenerator {
     *  integer between 0 and 10 will be added to the end of the name, e.g `focused_turing3`
     */
     public static String getRandomName(int retry) {
-        Random random = new Random();
-        String name = left[random.nextInt(left.length)] + "_" + right[random.nextInt(right.length)];
+        String name = left[RANDOM.nextInt(left.length)] + "_" + right[RANDOM.nextInt(right.length)];
 
         if (name.equals("boring_wozniak")) { // Steve Wozniak is not boring
             return getRandomName(retry);
         }
 
         if (retry > 0) {
-            name += random.nextInt(11);
+            name += RANDOM.nextInt(11);
         }
 
         return name;
