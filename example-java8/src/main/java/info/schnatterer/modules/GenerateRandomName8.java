1fd26ca3c2b960eb67a2ee5eeb80479cfb7f5645
Johannes Schnatterer <johannes@schnatterer.info>
2020-12-13 14:16:51 +0100

    Add compatibility for Java 8

---
+++ example-java8/src/main/java/info/schnatterer/modules/GenerateRandomName8.java
@@ -0,0 +1,12 @@
+package info.schnatterer.modules;
+
+import info.schnatterer.mobynamesgenerator.MobyNamesGenerator;
+
+public class GenerateRandomName8 {
+
+    public static void main(String[] args) {
+        //for (int i = 0; i < 1000000; i++) {
+            System.out.println(MobyNamesGenerator.getRandomName());
+        //}
+    }
+}
