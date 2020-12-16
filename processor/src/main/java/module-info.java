b5c7ffb475ba39cfb8ea0ca1c1fcaf1f50cc86d9
Johannes Schnatterer <johannes@schnatterer.info>
2020-12-16 23:22:39 +0100

    Introduce annotation processor the module way.
    "provides javax.annotation.processing.Processor" - keep compatibility even for JDK12+?!

--- processor/src/main/java/module-info.java
+++ processor/src/main/java/module-info.java
@@ -2,5 +2,6 @@ module info.schnatterer.mobynamesgenerator.processor {
     requires java.compiler;
     requires com.github.mustachejava;
 
+    provides javax.annotation.processing.Processor with info.schnatterer.mobynamesgenerator.processor.NamesGeneratorProcessor;
     exports info.schnatterer.mobynamesgenerator.processor;
 }
